affb180873b7c575436da37c636384578d874dd1
n1ssinnnn <128668407+n1ssinnnn@users.noreply.github.com>
2023-03-23 17:16:15 +0700

    Add files via upload

---
+++ Exercise4_Thanva_Y.py
@@ -0,0 +1,13 @@
+foundeng = float(50)
+generalbus = float(60)
+Introductiontocomsys = float(70)
+comPrograming = float(80)
+
+
+
+print("--- Your Score ---")
+print("Foundation English : ",foundeng,"pts")
+print("General Business : ",generalbus,"pts")
+print("Introduction to Computer Systems : ",Introductiontocomsys,"pts")
+print("Programming : ",comPrograming,"pts")
+
